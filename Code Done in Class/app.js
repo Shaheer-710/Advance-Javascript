4a09d20e42725de0b4b138556a0343bcc7593160
Muhammad Shaheer <shaheer7w@gmail.com>
2024-12-04 13:13:42 +0500

    Async/await Practice

--- Code Done in Class/app.js
+++ Code Done in Class/app.js
@@ -1,40 +1,25 @@
-let name1 = "shaheer"
-var name2 = "ali"
-
-var name2 = name1;
-
-name1 = "ahmed"
-
-console.log(name1);
-console.log(name2);
-
-
-let arr1 = [1 , 2, 3, 4,5]
-let arr2 =  [...arr1]
-
-arr2.push("twenty 1")
-
-console.log(arr2);
-console.log(arr1);
-
-let obj1 ={
-    email:"shaheer7w@gmail.com",
-    password:12346578,
-    extradetails:{
-        id : 12345,
-        status:false
-    }
-}
-
-
-
-let obj2 = {...obj1 , education:{
-    ...obj1.extradetails
-}}
-
-obj2.password= 123456
-obj2.extradetails.id =23456
-
-console.log(obj1);
-console.log(obj2);
-
+const Timecontrol = async () => {
+    const KarachiSunsetPromise = new Promise(resolve => {
+      setTimeout(() => {
+        alert('The time for Sunset in Karachi is 6:00 PM');
+        resolve();
+      }, 4000);
+    });
+  
+    const LahoreSunsetPromise = new Promise(resolve => {
+      setTimeout(() => {
+        alert('The time for sunset in Lahore is 5:50 PM');
+        resolve();
+      }, 5000);
+    });
+  
+    console.log('Karachi sunset time loading');
+    await KarachiSunsetPromise;
+    console.log('Karachi Sunset Time loaded');
+  
+    console.log('Lahore sunset time loading');
+    await LahoreSunsetPromise;
+    console.log('Lahore sunset time loaded');
+  };
+  
+  Timecontrol();
