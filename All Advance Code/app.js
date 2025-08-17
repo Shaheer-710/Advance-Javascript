8773cfd176e9dd6b020b66ad2be6795a5246d048
Muhammad Shaheer <shaheer7w@gmail.com>
2025-08-17 17:22:46 +0500

    Task 1

--- All Advance Code/app.js
+++ All Advance Code/app.js
@@ -1,120 +1,38 @@
-let a = 5;
+// Task 1 
 
-let exp1 = ((4 + 5) && "abc" || false + "test") * (a-- || --a) || (false && (++a + 1)) * "end";
-console.log(exp1);
-let exp2 = 10 * ("foo" && 5 + (++a) || "bar") && (false + "test") || 0 && true;
-let exp3 = 3 + (a-- || "start") * 4 && (--a + "value") * (1 + 2) + "result";
-let exp4 = "hello" * (++a + true) || (2 + 3 * "abc") * (0 + 1) + "xyz" && 0;
-let exp5 = (true || (0 + 1) * "test" && 4) || (false && 3 + "value") * a-- + 2;
-let exp6 = ++a + "abc" && (4 * 2) + 3 || (0 + 1) && (3 * "hello") || a--;
-let exp7 = ("foo" + 5) * (++a + true) || 2 * 3 + (true + 2) || "result";
-let exp8 = (0 + 1) && (true + 0) || (false + "test") * 4 && 3 + 2 || "value";
-let exp9 = 3 * "abc" + (true + 1) || (++a + "test") && (3 + "result") || null;
-let exp10 = (++a + false) && "start" || 0 + 1 && "value" || 5 * "end" && a++;
-let exp11 = (false && "hello") + (a++ || 3 + "test") * 4 || 5 * "abc" && 0;
-let exp12 = "hello" * (true + 0) + 2 || (false + 1) * 3 && "result" || 4 + "test";
-let exp13 = 5 * (a++ || false) + 2 && (false + "test") || 3 * "end" && 4;
-let exp14 = (false + "abc") * 2 || (--a + 1) * "start" + 3 && 4 || "end";
-let exp15 = (0 + "foo") * 3 + (true && "result") || "start" + (++a + 1) * 4;
-let exp16 = 2 * "end" || (false && true) || "start" + (--a + 2) * 5 && null;
-let exp17 = 3 + 2 * ("test" + a--) && (false + 3) * 5 || 0 + "value" && 4;
-let exp18 = "start" && (false || 2 * "end") || (++a + 1) * 3 + "result" && 0;
-let exp19 = ((5 + 1) && "foo") || (++a + 2) * (false + 3) + "test" && 7;
-let exp20 = 2 * 3 + "hello" && (false + ++a) * "result" || "end" + 5 || 0;
-let exp21 = 5 * (true + ++a) && ("test" + false) || 7 * (true + 2) + "value";
-let exp22 = "foo" + 4 && (++a + 1) * "start" || 5 + 6 * (false + true) && "test";
-let exp23 = (false && 2) || (a++ + 1) * "end" && "start" || 4 * 5 && "result";
-let exp24 = 3 + 2 * "test" || (false + a--) * "hello" && "world" + 1 || 2;
-let exp25 = (3 + 4) * (false || a--) && 5 || "start" + 1 + "test" && 0;
-let exp26 = "hello" && 3 * 2 + (a++ + 1) || (false + true) * "result" + "end";
-let exp27 = 3 * "test" + (true + 2) && (false || "value") || "start" + a++;
-let exp28 = (false + 1) * "hello" || 3 + (a-- && 5) * "result" || "world";
-let exp29 = "start" + 2 * (true || 1) && (false || "value") * 5 + "result";
-let exp30 = (true + 3) * "test" || 1 * 5 && (false + "value") + "end" || a--;
-let exp31 = 3 + "end" || 2 * "test" && (++a + true) || "start" + 1;
-let exp32 = (0 + 3) * (true + false) || 5 * "hello" + 2 && (false + "test");
-let exp33 = 2 + 3 && ("end" + a++) || (false + "test") * 4 && 5;
-let exp34 = "hello" + 4 * (false + a--) || 3 && "start" + 1 || (true + "test");
-let exp35 = "start" && (a-- || "test") * 4 + 5 && (false + "end") || 2;
-let exp36 = 1 + "value" && (++a + 2) || (3 + "result") * true && 4;
-let exp37 = "hello" && 2 + "test" || (++a + 3) && (true + "value") + 1;
-let exp38 = 5 * (a-- || "test") && 6 * "result" || 2 + "end";
-let exp39 = "start" && (false + 1) * 2 || 3 + 4 * "hello" + 5 && 0;
-let exp40 = (false || "test") * 5 || 6 + (a-- && "result") * 4;
-let exp41 = "start" && (3 + 2) * "test" + 5 || 4 * (false + 1) && "hello";
-let exp42 = 1 + 2 * "end" || (false + 3) && "result" * 4 + a--;
+// let name = "Shaheer"
+// let age = 16
+// let occupation = "web developer"
+// let email = 'shaheer7w@gmail.com'
+// let phone = "021322323282"
 
-let exp43 = (false && a--) || 4 * (3 + 2) && "start" + 5;
-let exp44 = 3 + 2 * (true + 5) && "value" + 1 || (++a + 2) + "test";
-let exp45 = (false || 1) + "test" && 5 + (3 * a--) || "end" + 2;
-let exp46 = (2 * a-- + 4) && "test" || 3 + "hello" && (false + 1) * 5;
-let exp47 = 0 + "result" && (3 + 1) * 2 || (false + a--) * "end";
-let exp48 = (false || 1) * "test" && 4 || (true + 2) * "hello" + a--;
-let exp49 = (2 * 3) + "result" && 4 * (a-- + 1) || "start" + 2 + "end";
-let exp50 = 32 && true - ++a && " " || "true"; // for a = 5
-let exp51 = (5 + 2) * (a-- + 1) || "start" + (++a + "end") * 3;
-let exp52 = (++a && 3) * "test" || 4 + "start" * (a-- + "result");
-let exp53 = 3 + "value" * (++a + 1) || (a-- && "start") + "end";
-let exp54 = (a-- + 2) * "result" || (false && 5) * "test" + 4;
-let exp55 = "start" + 5 * (a-- + "test") || (false + 2) * "value";
-let exp56 = 4 * (a-- + 1) + "test" || (++a + 3) * "start" + 5;
-let exp57 = (3 * "test" + 1) || (++a && a--) * "result" || "value";
-let exp58 = (a-- + "start") * "result" || (false + 2) + "end" + 3;
 
-console.log(exp1);
-console.log(exp2);
-console.log(exp3);
-console.log(exp4);
-console.log(exp5);
-console.log(exp6);
-console.log(exp7);
-console.log(exp8);
-console.log(exp9);
-console.log(exp10);
-console.log(exp11); 1 
-console.log(exp12);
-console.log(exp13);
-console.log(exp14);
-console.log(exp15);
-console.log(exp16);
-console.log(exp17);
-console.log(exp18);
-console.log(exp19);
-console.log(exp20);
-console.log(exp21);
-console.log(exp22);
-console.log(exp23);
-console.log(exp24);
-console.log(exp25);
-console.log(exp26);
-console.log(exp27);
-console.log(exp28);
-console.log(exp29);
-console.log(exp30);
-console.log(exp31);
-console.log(exp32);
-console.log(exp33);
-console.log(exp34);
-console.log(exp35);
-console.log(exp36);
-console.log(exp37);
-console.log(exp38);
-console.log(exp39);
-console.log(exp40);
-console.log(exp41);
-console.log(exp42);
-console.log(exp43);
-console.log(exp44);
-console.log(exp45);
-console.log(exp46);
-console.log(exp47);
-console.log(exp48);
-console.log(exp49);
-console.log(exp50);
-console.log(exp51);
-console.log(exp52);
-console.log(exp53);
-console.log(exp54);
-console.log(exp55);
-console.log(exp56);
-console.log(exp57);
+// let data = `Name:${name} <br>
+//              <hr>
+//                 Age:${age} <br>
+//                 <hr>
+//                 Occupation:${occupation} <br>
+//                 <hr>
+//                 Email:${email}<br>
+//                 <hr>
+//                 Phone:${phone} <br>`
+
+// document.write(data)
+
+// Task 2
+
+// var name = "Ahmed"
+// var name="ali"     // CAN BE REASSIGNED
+// let work = "full stack developer"
+// // let work = "full stack developer"  can not be reasigned and shows error
+// work = 'web designer' //can be reasgned if the variable name is directed
+// const salary = "1233333" 
+// // const salary = "dsdd"  can not be reasigned and shows error
+// salary=323234 //Assignment to constant variable. can not be reasigned
+
+
+// console.log(name);
+// console.log(work)
+// console.log(salary)
+
+
